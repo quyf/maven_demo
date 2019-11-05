6a922eefea89c65d65c84fbfeb394b4320390c20
quyf <544529278@qq.com>
2019-11-05 20:51:09 +0800

    springboot pom
    提交

--- src/main/java/cn/quyf/mq/activemq/spring/Consumer.java
+++ src/main/java/cn/quyf/mq/activemq/spring/Consumer.java
@@ -1,34 +1,34 @@
-package cn.quyf.mq.activemq.spring;
-
-import javax.annotation.Resource;
-import javax.jms.Destination;
-import javax.jms.JMSException;
-import javax.jms.TextMessage;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(value = "/mq/activemq/spring/spring-core.xml")
-public class Consumer {
-
-	@Resource(name = "JmsTemplate")
-	private JmsTemplate jmsTemplate;
-
-	@Resource(name = "queueDestination")
-	private Destination destination;
-
-	@Test
-	public void test() {
-		System.out.println(jmsTemplate);
-		TextMessage message = (TextMessage) jmsTemplate.receive(destination);
-		try {
-			System.out.println("接受消息>>>>>>>>>>>" + message.getText());
-		} catch (JMSException e) {
-			e.printStackTrace();
-		}
-	}
-}
+//package cn.quyf.mq.activemq.spring;
+//
+//import javax.annotation.Resource;
+//import javax.jms.Destination;
+//import javax.jms.JMSException;
+//import javax.jms.TextMessage;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.jms.core.JmsTemplate;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(value = "/mq/activemq/spring/spring-core.xml")
+//public class Consumer {
+//
+//	@Resource(name = "JmsTemplate")
+//	private JmsTemplate jmsTemplate;
+//
+//	@Resource(name = "queueDestination")
+//	private Destination destination;
+//
+//	@Test
+//	public void test() {
+//		System.out.println(jmsTemplate);
+//		TextMessage message = (TextMessage) jmsTemplate.receive(destination);
+//		try {
+//			System.out.println("接受消息>>>>>>>>>>>" + message.getText());
+//		} catch (JMSException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//}
